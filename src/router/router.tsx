ce559ae904d9c79afef5b9e5c216cfe41ccb8052
lucas93220 <lopezlucas52@gmail.com>
2025-11-21 13:26:26 +0100

    création de la page resultat des quizz classique

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,12 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import ClassicQuizzPage from "../pages/classic-quizz-page";
 import DifficultySelection from "../pages/difficulty-selection";
 import Homepage from "../pages/Homepage";
 import QuizzSelection from "../pages/quizz-selection";
+import ClassicQuizzResults from "../pages/results-classic-quizz";
 import ResultsSuddenDeath from "../pages/results-sudden-death";
 import SuddenDeath from "../pages/sudden-death";
 import ThemeSelection from "../pages/theme-selection";
-import ClassicQuizzPage from "../pages/classic-quizz-page";
 
 export default function Router() {
   return (
@@ -16,9 +17,9 @@ export default function Router() {
         <Route path="/theme-selection" element={<ThemeSelection />} />
         <Route path="/difficulty-selection" element={<DifficultySelection />} />
         <Route path="/sudden-death" element={<SuddenDeath />} />
-        <Route path="/results" element={<ResultsSuddenDeath />} />
+        <Route path="/results-sudden-death" element={<ResultsSuddenDeath />} />
         <Route path="/classic-quizz" element={<ClassicQuizzPage />} />
-
+        <Route path="/results-classic" element={<ClassicQuizzResults />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>

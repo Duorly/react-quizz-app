3cb165a8752bc688b2e3febb4b2d4803f820d71b
lucas93220 <lopezlucas52@gmail.com>
2025-12-10 16:59:09 +0100

    LoadingScreen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import backgroundVideo from "../assets/Background_animated.mp4";
-
+import Background from "./Background";
 interface LoadingScreenProps {
     message?: string;
 }
@@ -10,14 +9,7 @@ const LoadingScreen: React.FC<LoadingScreenProps> = ({
 }) => {
     return (
         <div className="w-full h-[100dvh] relative flex items-center justify-center bg-black overflow-hidden">
-            <video
-                src={backgroundVideo}
-                autoPlay
-                loop
-                muted
-                playsInline
-                className="absolute inset-0 w-full h-full object-cover opacity-50"
-            />
+            <Background />
             <div
                 className="relative z-10 text-white text-4xl animate-pulse font-bold"
                 style={{ fontFamily: "'Jomhuria', cursive" }}

374cabff58f19d3e6fad4ded5b54e7c0f083b005
lucas93220 <lopezlucas52@gmail.com>
2025-12-10 15:44:53 +0100

    ResultText

---
+++ src/components/result/ResultText.tsx
@@ -0,0 +1,12 @@
+const ResultText = () => (
+  <p
+    className="text-white drop-shadow-lg mt-4 sm:mt-6
+               text-4xl sm:text-5xl md:text-6xl lg:text-[70px]
+               leading-tight"
+    style={{ fontFamily: "'Jomhuria', cursive" }}
+  >
+    Vous avez obtenu le résultat de :
+  </p>
+);
+
+export default ResultText;
